534b809a15bcae42150b2e6bcca16fcbca88bd92
Cauã Rinaldi <cauarinaldi451@gmail.com>
2021-12-14 13:51:41 -0300

    Add injection on methods named Setup

---
+++ SimplestUnityDI/Baking/BakedMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace SimplestUnityDI.Baking
+{
+    public class BakedMethod
+    {
+        public Func<object, object[], object> Action { get; }
+        public BakedParameter[] Parameters { get; }
+
+        public BakedMethod(MethodInfo info)
+        {
+            Action = info.Invoke;
+            Parameters = BakedParameter.BakeParameters(info);
+        }
+    }
+}
--- SimplestUnityDI/DiMonoBehaviour.cs
+++ SimplestUnityDI/DiMonoBehaviour.cs
@@ -1,14 +1,38 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using JetBrains.Annotations;
+using SimplestUnityDI.Baking;
+using UnityEngine;
 
 namespace SimplestUnityDI
 {
     public abstract class DiMonoBehaviour : MonoBehaviour
     {
+        private static readonly IDictionary<Type, BakedMethod> BakedSetups = new Dictionary<Type, BakedMethod>();
+
         private void Awake()
         {
-            AfterInjection();
+            DiContainer container = DiContainer.Instance;
+            Type type = GetType();
+
+            if (!BakedSetups.TryGetValue(type, out BakedMethod method))
+            {
+                MethodInfo setupInfo = type.GetRuntimeMethods().FirstOrDefault(o => o.Name == "Setup");
+                if (setupInfo is null)
+                    throw new InvalidOperationException($"Type {type.FullName} must declare a method named Setup");
+
+                method = new BakedMethod(setupInfo);
+                BakedSetups[type] = method;
+            }
+
+            method.Action(this, container.ResolveParameters(method.Parameters));
         }
 
+        /// <summary>
+        /// Will execute after dependency injection happens, on Awake
+        /// </summary>
         protected virtual void AfterInjection() { }
     }
 }
